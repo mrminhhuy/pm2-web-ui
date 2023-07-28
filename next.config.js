1b81a1e3d8463c4191b2f3509805b4e316f238cd
Minh Huy <huyvm@e-plus.vn>
2023-07-28 16:33:50 +0700

    test

--- next.config.js
+++ next.config.js
@@ -1,14 +1,3 @@
 const withCSS = require('@zeit/next-css');
 
-// module.exports = withCSS();
-
-module.exports = {
-    async rewrites() {
-        return [
-            {
-                source: '/',
-                destination: '/cicd'
-            }
-        ]
-    }
-}
+module.exports = withCSS();
