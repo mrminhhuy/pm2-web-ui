aa71c395adcf73be0556f266863bc26d79abd6a2
Minh Huy <huyvm@e-plus.vn>
2023-07-28 15:56:22 +0700

    test

--- next.config.js
+++ next.config.js
@@ -1,3 +1,23 @@
 const withCSS = require('@zeit/next-css');
 
-module.exports = withCSS();
+// module.exports = withCSS();
+
+module.exports = {
+    async rewrites() {
+        return [
+            {
+                source: '/api',
+                destination: '/cicd/api'
+            },
+            {
+                source: '/bulma',
+                destination: '/cicd/bulma'
+            },
+            {
+                source: '/img',
+                destination: '/cicd/img'
+            }
+        ]
+    },
+    assetPrefix: 'https://ttttykhoa-staging.e-plus.vn/cicd'
+}
