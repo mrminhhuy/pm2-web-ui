a33a2cc34bcf41f84bd98f46b5775308e1d174c6
Minh Huy <huyvm@e-plus.vn>
2023-08-01 12:04:49 +0700

    test

--- src/modules/tuysf/tuysf.service.ts
+++ src/modules/tuysf/tuysf.service.ts
@@ -8,7 +8,7 @@ export class TuysfService {
 
   async executeDeploy(phone: string) {
     var cp = require('child_process');
-    cp.exec('./root/b.sh', function(err, stdout, stderr) {
+    cp.exec('/root/b.sh', function(err, stdout, stderr) {
       console.log('err', err);
       console.log('stdout', stdout);
       console.log('stderr', stderr);
